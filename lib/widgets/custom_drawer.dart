f276b248a968d01aad0374762222b93fe314ddc6
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-14 10:27:41 -0300

    add drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,8 +1,80 @@
+import 'package:ecommerce/tiles/drawerTile.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatelessWidget {
+  final PageController pageController;
+
+  CustomDrawer(this.pageController);
+
+  Widget _buildDrawerBack() => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color.fromRGBO(255, 200, 77, 1), Colors.white],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: Stack(
+        children: <Widget>[
+          _buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 18.0),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.fromLTRB(0.0, 16, 16, 8),
+                height: 170,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: 8,
+                      left: 0,
+                      child: Text(
+                        'Minha\nLoja',
+                        style: TextStyle(
+                            fontSize: 35, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Olá,',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              'Entre ou cadastre-se',
+                              style: TextStyle(
+                                  color: Colors.amber[800],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {},
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Divider(),
+              DrawerTile(Icons.home, "Início", pageController, 0),
+              DrawerTile(Icons.list, "Produtos", pageController, 1),
+              DrawerTile(
+                  Icons.playlist_add_check, "Meus Pedidos", pageController, 2),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
