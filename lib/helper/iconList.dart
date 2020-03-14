da508dfa16aecfab927b2e34f4bfb3c7388e8e6e
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-14 12:43:01 -0300

    add icon list

---
+++ lib/helper/iconList.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Map<String, IconData> iconList = {
+  'Celulares': FontAwesomeIcons.mobileAlt,
+  'Eletrônicos': FontAwesomeIcons.laptop,
+  'Maquiagem': FontAwesomeIcons.magic,
+  'Livros': FontAwesomeIcons.book,
+  'Relógios': FontAwesomeIcons.clock,
+  'Perfumes': Icons.local_drink,
+};
