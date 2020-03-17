566f4d25186befee30609393e67cf1dd84c281e1
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-17 01:11:23 -0300

    Update product tile

--- lib/tiles/productTile.dart
+++ lib/tiles/productTile.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/Screens/product_screen.dart';
 import 'package:ecommerce/modal/product_modal.dart';
 import 'package:flutter/material.dart';
 
@@ -10,31 +11,83 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ProductScreen(product),
+          ),
+        );
+      },
       child: Card(
-        child: Column(
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 0.9,
-              child: Image.network(
-                product.images[1],
-                fit: BoxFit.cover,
-              ),
-            ),
-            Text(product.title),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    'preço: R\$ ${product.price.toString()}',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+          child: type == 'grid'
+              ? Column(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 0.9,
+                      child: Image.network(
+                        product.images[0],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(product.title),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Preço: R\$ ${product.price.toStringAsFixed(2)}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Row(
+                  children: <Widget>[
+                    Flexible(
+                      flex: 1,
+                      child: Image.network(
+                        product.images[0],
+                        fit: BoxFit.contain,
+                        height: 250,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(product.title),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    'Preço: R\$ ${product.price.toStringAsFixed(2)}',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )),
     );
   }
 }
