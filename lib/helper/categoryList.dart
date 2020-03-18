7bd7933359ccdc98e421c9875d2fc54e32e7a28d
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-17 23:56:41 -0300

    update category list

--- lib/helper/categoryList.dart
+++ lib/helper/categoryList.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Map<String, AssetImage> imageCategoryList = {
-  'Celulares': AssetImage('assets/images/celularCategory.png'),
-  'Eletrônicos': AssetImage('assets/images/celularCategory.png'),
-  'Maquiagem': AssetImage('assets/images/celularCategory.png'),
-  'Livros': AssetImage('assets/images/celularCategory.png'),
-  'Relógios': AssetImage('assets/images/celularCategory.png'),
-  'Perfumes': AssetImage('assets/images/celularCategory.png'),
+  'Eletrônicos': AssetImage('assets/images/eletronicosCategory.png'),
+  'Maquiagem': AssetImage('assets/images/maquiagemCategory.png'),
+  'Livros': AssetImage('assets/images/booksCategory.png'),
+  'Perfumes': AssetImage('assets/images/perfumeCategory.png'),
 };
