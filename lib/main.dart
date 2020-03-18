3a3466e38ce3dcbd93bc884054b49b8c480ab7fd
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-18 18:53:27 -0300

    add scoped model

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:ecommerce/Screens/home_screen.dart';
+import 'package:ecommerce/Screens/login_screen.dart';
+import 'package:ecommerce/Screens/signup_screen.dart';
+import 'package:ecommerce/modal/user_modal.dart';
+import 'package:ecommerce/tabs/home_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
     );
   }
 }
