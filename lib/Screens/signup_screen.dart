5728d855982d1f52a91043d33567ae423d27d350
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-18 20:04:08 -0300

    create user firebase

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -1,7 +1,19 @@
+import 'package:ecommerce/modal/user_modal.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _adressController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,70 +25,100 @@ class SignUpScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(hintText: 'Nome Completo*'),
-                validator: (text) {
-                  if (text.isEmpty || text.length < 5) return 'Nome inválido';
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(hintText: 'E-mail*'),
-                validator: (text) {
-                  if (text.isEmpty || !text.contains('@'))
-                    return "E-mail inválido";
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(hintText: 'Senha*'),
-                validator: (text) {
-                  if (text.isEmpty || text.length < 6) return 'Senha inválida';
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(hintText: 'Endereço*'),
-                validator: (text) {
-                  if (text.isEmpty || text.length < 6)
-                    return 'Endereço invalido';
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  'CADASTRAR',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(hintText: 'Nome Completo*'),
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 5)
+                        return 'Nome inválido';
+                    },
+                  ),
                 ),
-              ),
-              color: Colors.cyan[600],
-              onPressed: () {
-                if (_formKey.currentState.validate()) {}
-              },
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(hintText: 'E-mail*'),
+                    validator: (text) {
+                      if (text.isEmpty || !text.contains('@'))
+                        return "E-mail inválido";
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _passController,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Senha*'),
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 6)
+                        return 'Senha inválida';
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _adressController,
+                    decoration: InputDecoration(hintText: 'Endereço*'),
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 6)
+                        return 'Endereço invalido';
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'CADASTRAR',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  color: Colors.cyan[600],
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Map<String, dynamic> userData = {
+                        "name": _nameController.text,
+                        "email": _emailController.text,
+                        "adress": _adressController.text
+                      };
+
+                      model.signUp(
+                          userData: userData,
+                          pass: _passController.text,
+                          onSucess: _onSucess,
+                          onFail: _onFail);
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+
+  void _onSucess() {}
+
+  void _onFail() {}
 }
--- lib/modal/user_modal.dart
+++ lib/modal/user_modal.dart
@@ -1,9 +1,44 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseUser firebaseUser;
+
+  Map<String, dynamic> userData = Map();
+
   bool isLoading = false;
 
-  void signUp() {}
+  void signUp(
+      {@required Map<String, dynamic> userData,
+      @required String pass,
+      @required VoidCallback onSucess,
+      @required VoidCallback onFail}) {
+    isLoading = true;
+    notifyListeners();
+
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData["email"], password: pass)
+        .then((user) async {
+      firebaseUser = user.user;
+
+      await _saveUserData(userData);
+
+      onSucess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
+  }
 
   void signIn() async {
     isLoading = true;
@@ -15,6 +50,14 @@ class UserModel extends Model {
   }
 
   void reconverPass() {}
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .setData(userData);
+  }
 
   bool isLogged() {}
 }
