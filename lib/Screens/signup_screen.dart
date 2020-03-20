f6f0b84f5de730834fd14811acd3d6ede989a44d
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-19 23:58:43 -0300

    add single child list

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -33,84 +33,96 @@ class _SignUpScreenState extends State<SignUpScreen> {
             return Center(
               child: CircularProgressIndicator(),
             );
-          return Form(
-            key: _formKey,
-            child: ListView(
-              padding: EdgeInsets.all(16.0),
+          return SingleChildScrollView(
+            child: Column(
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(hintText: 'Nome Completo*'),
-                    validator: (text) {
-                      if (text.isEmpty || text.length < 5)
-                        return 'Nome inválido';
-                    },
-                  ),
+                Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/image/iconAddUser.png'))),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(hintText: 'E-mail*'),
-                    validator: (text) {
-                      if (text.isEmpty || !text.contains('@'))
-                        return "E-mail inválido";
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: _passController,
-                    obscureText: true,
-                    decoration: InputDecoration(hintText: 'Senha*'),
-                    validator: (text) {
-                      if (text.isEmpty || text.length < 6)
-                        return 'Senha inválida';
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: _adressController,
-                    decoration: InputDecoration(hintText: 'Endereço*'),
-                    validator: (text) {
-                      if (text.isEmpty || text.length < 6)
-                        return 'Endereço invalido';
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      'CADASTRAR',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                  ),
-                  color: Colors.cyan[600],
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Map<String, dynamic> userData = {
-                        "name": _nameController.text,
-                        "email": _emailController.text,
-                        "adress": _adressController.text
-                      };
+                Form(
+                  key: _formKey,
+                  child: ListView(
+                    padding: EdgeInsets.all(16.0),
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _nameController,
+                          decoration:
+                              InputDecoration(hintText: 'Nome Completo*'),
+                          validator: (text) {
+                            if (text.isEmpty || text.length < 5)
+                              return 'Nome inválido';
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(hintText: 'E-mail*'),
+                          validator: (text) {
+                            if (text.isEmpty || !text.contains('@'))
+                              return "E-mail inválido";
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _passController,
+                          obscureText: true,
+                          decoration: InputDecoration(hintText: 'Senha*'),
+                          validator: (text) {
+                            if (text.isEmpty || text.length < 6)
+                              return 'Senha inválida';
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _adressController,
+                          decoration: InputDecoration(hintText: 'Endereço*'),
+                          validator: (text) {
+                            if (text.isEmpty || text.length < 6)
+                              return 'Endereço invalido';
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            'CADASTRAR',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                        color: Colors.cyan[600],
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            Map<String, dynamic> userData = {
+                              "name": _nameController.text,
+                              "email": _emailController.text,
+                              "adress": _adressController.text
+                            };
 
-                      model.signUp(
-                          userData: userData,
-                          pass: _passController.text,
-                          onSucess: _onSucess,
-                          onFail: _onFail);
-                    }
-                  },
+                            model.signUp(
+                                userData: userData,
+                                pass: _passController.text,
+                                onSucess: _onSucess,
+                                onFail: _onFail);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
