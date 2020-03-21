dec34d64d05299bc08ee36498893bcfad82dc649
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-20 22:38:51 -0300

    add scoped model

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/modal/user_modal.dart';
 import 'package:ecommerce/tiles/category_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class HomeTab extends StatefulWidget {
   @override
@@ -20,20 +22,66 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-      future: Firestore.instance.collection("products").getDocuments(),
-      builder: (context, snapshop) {
-        if (!snapshop.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          return ListView(
-            children: snapshop.data.documents.map((doc) {
-              return CategoryTile(doc);
-            }).toList(),
-          );
-        }
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, model) {
+        return FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance.collection("products").getDocuments(),
+          builder: (context, snapshop) {
+            if (!snapshop.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Column(
+                children: <Widget>[
+                  !model.isLogged()
+                      ? Row(
+                          children: <Widget>[],
+                        )
+                      : Container(
+                          padding: EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'olá, ${!model.isLoggedIn() ? "" : model.userData["name"]}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 20),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(top: 8),
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      'Saldo disponível:',
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.cyan),
+                                    ),
+                                    Text(
+                                      'R\$:0,00',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 20,
+                                          color: Colors.cyan[600]),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                  Expanded(
+                    child: ListView(
+                      children: snapshop.data.documents.map((doc) {
+                        return CategoryTile(doc);
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        );
       },
     );
   }
