fdf09ce254905d8e07bfd41aa6d7ebaffa090dda
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-08 19:23:29 -0300

    create cartTile

---
+++ lib/tiles/cartTile.dart
@@ -0,0 +1,113 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/modal/cart_modal.dart';
+import 'package:ecommerce/modal/cart_product.dart';
+import 'package:ecommerce/modal/product_modal.dart';
+import 'package:flutter/material.dart';
+
+class CartTile extends StatelessWidget {
+  final CartProduct cartProduct;
+
+  CartTile(this.cartProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildContent() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      cartProduct.productData.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 17),
+                    ),
+                  ),
+                  Text(
+                    'Tamanho: ${cartProduct.colors}',
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    'Preço: ${cartProduct.productData.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: cartProduct.quantity > 1
+                            ? () {
+                                CartModel.of(context).decProduct(cartProduct);
+                              }
+                            : null,
+                      ),
+                      Text(cartProduct.quantity.toString()),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          CartModel.of(context).incProduct(cartProduct);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text('Remover'),
+                        textColor: Colors.grey[500],
+                        onPressed: () {
+                          CartModel.of(context).removeCartItem(cartProduct);
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Card(
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: cartProduct.productData == null
+            ? FutureBuilder<DocumentSnapshot>(
+                future: Firestore.instance
+                    .collection("products")
+                    .document(cartProduct.category)
+                    .collection("items")
+                    .document(cartProduct.pid)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    cartProduct.productData =
+                        ProductModal.fromDocument(snapshot.data);
+                    return _buildContent();
+                  } else {
+                    return Container(
+                      height: 70,
+                      child: CircularProgressIndicator(),
+                      alignment: Alignment.center,
+                    );
+                  }
+                },
+              )
+            : _buildContent());
+  }
+}
