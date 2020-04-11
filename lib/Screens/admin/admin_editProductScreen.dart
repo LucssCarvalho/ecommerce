cc3267d9443a836b35b1e7a0575127b52c71864c
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-11 13:40:07 -0300

    create admin_editProductScreen

---
+++ lib/Screens/admin/admin_editProductScreen.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatelessWidget {
+  final String categoryId;
+  final DocumentSnapshot document;
+
+  EditProductScreen(this.categoryId, this.document);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
