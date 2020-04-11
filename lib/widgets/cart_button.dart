641cdc252c512c462df000d39760821afd59acbb
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-11 20:25:58 -0300

    update button

--- lib/widgets/cart_button.dart
+++ lib/widgets/cart_button.dart
@@ -1,18 +1,34 @@
 import 'package:ecommerce/Screens/cart_screen.dart';
+import 'package:ecommerce/modal/cart_modal.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class CartButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       backgroundColor: Colors.blue[800],
-      child: Icon(
-        Icons.shopping_cart,
-        color: Colors.white,
+      child: Container(
+        alignment: Alignment.center,
+        child: ScopedModelDescendant<CartModel>(
+          builder: (context, child, model) {
+            int quantProducts = model.products.length;
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                quantProducts != 0 ? Text(quantProducts.toString()) : Text(''),
+                Icon(Icons.shopping_cart)
+              ],
+            );
+          },
+        ),
       ),
       onPressed: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => CartScreen()));
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => CartScreen(),
+          ),
+        );
       },
     );
   }
