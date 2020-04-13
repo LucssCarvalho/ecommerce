a54470dfd68ed26dd4e071896fe9d6b28d68c90e
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-13 15:04:01 -0300

    create question_tab

---
+++ lib/tabs/question_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionTab extends StatefulWidget {
+  @override
+  _QuestionTabState createState() => _QuestionTabState();
+}
+
+class _QuestionTabState extends State<QuestionTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[],
+    );
+  }
+}
