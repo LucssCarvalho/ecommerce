fe15497ef1f2d7f13757b15c2b1e20626acf346f
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-13 15:04:21 -0300

    create term_of_Use

---
+++ lib/tabs/term_of_use.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TermOfUseTab extends StatefulWidget {
+  @override
+  _TermOfUseTabState createState() => _TermOfUseTabState();
+}
+
+class _TermOfUseTabState extends State<TermOfUseTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[],
+    );
+  }
+}
